6385f8ccd749bb75f45b3f6fea5287633620a9bc
Vitaliy <vitvegl@quintagroup.org>
2015-11-17 14:07:39 +0200

    temporary_directory patch

--- lib/logstash/outputs/s3.rb
+++ lib/logstash/outputs/s3.rb
@@ -101,7 +101,7 @@ class LogStash::Outputs::S3 < LogStash::Outputs::Base
 
   # Set the directory where logstash will store the tmp files before sending it to S3
   # default to the current OS temporary directory in linux /tmp/logstash
-  config :temporary_directory, :validate => :string, :default => File.join(Dir.tmpdir, "logstash")
+  config :temporary_directory, :validate => :string, :default => "/var/lib/logstash/S3_temp/"
 
   # Specify a prefix to the uploaded filename, this can simulate directories on S3
   config :prefix, :validate => :string, :default => ''
